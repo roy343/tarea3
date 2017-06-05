8a579a0213eaf2c7767c8990ba28442963988f47
Roy416 <roychav99@gmail.com>
2017-06-05 17:42:21 -0600

    Se agrego el archivo tren.py

---
+++ Tren.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+import os
+import time
+from threading import Thread
+import threading
+import winsound
+
+ventana = Tk()
+ventana.title("TX-343")
+ventana.geometry("1700x900+50+50")
+ventana.resizable(width=NO,height=NO)
