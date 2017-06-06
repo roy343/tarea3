142b71b79d9be01c04ca194e57c5554902336e5e
Roy416 <roychav99@gmail.com>
2017-06-05 20:52:29 -0600

    Clases

--- Tren.py
+++ Tren.py
@@ -23,9 +23,69 @@ boton4 = Button(ventana, text="Administración de vagones", bg = "#000000", fg =
 boton5 = Button(ventana, text="Salida de tren", bg = "#000000", fg = "#FFFFFF").place(x=2,y=123)
 boton6 = Button(ventana, text="Llegada de tren", bg = "#000000", fg = "#FFFFFF").place(x=2,y=153)
 
+class Tren:
+    def __init__(self,num,ruta,hora,maq,vag):
+        self.num=num
+        self.ruta=ruta
+        self.hora=hora
+        self.maquna=maq
+        self.vagon=vag
+        
+    def get_num(self):
+        return self.num
+    def set_num(self,num):
+        self.num=num
+        
+    def get_ruta(self):
+        return self.ruta
+    def set_ruta(self,ruta):
+        self.ruta=ruta
+        
+    def get_hora(self):
+        return self.hora
+    def set_hora(self,hora):
+        self.hora=hora
+        
+    def get_maq(self):
+        return self.maq
+    def set_maq(self,maq):
+        self.maq=maq
 
+    def get_vag(self):
+        return self.vag
+    def set_hora(self,vag):
+        self.vag=vag
 
+class Maquina:
+    def __init__(nume,cap):
+        Tren.__init__(self,num,ruta,hora,maq,vag)
+        nume=nume
+        cap=cap
+        
+    def get_nume(self):
+        return self.nume
+    def set_nume(self,nume):
+        self.num=nume
 
+    def get_cap(self):
+        return self.cap
+    def set_cap(self,cap):
+        self.cap=cap
 
+class Vagon:
+    def Tren__init__(num,cant,next=None,prev=None):
+        Tren.__init__(self,num,ruta,hora,maq,vag)
+        numo=numo
+        cant=cant
+    
+    def get_numo(self):
+        return self.numo
+    def set_numo(self,numo):
+        self.numo=numo
 
+    def get_cant(self):
+        return self.cant
+    def set_cant(self,cant):
+        self.cant=cant
 
+        
